bf7e65a4206d10099e7a88927d44173f63f02651
Egor <egorurtaev9824@gmail.com>
2023-05-30 19:39:54 +0300

    Blog page added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<nav>
+    <div class="nav-container"></div>
+    <a href="/" class="nav-logo" title="Go to Homepage">Егор</a>
+    {#each nav as link}
+        {#if link.isShow}
+            <a href="{link.href}" class="link">{link.title}</a>
+        {/if}
+    {/each}
+</nav>
+<div class="container">
+    <slot></slot>
+</div>
+
+<script>
+    const nav = [
+        {title: "Обо мне", href: "/about", isShow: true},
+        {title: "Блог", href: "/blog", isShow: true},
+        {title: "Мои проекты", href: "/projects", isShow: true},
+        {title: "Контакты", href: "/contacts", isShow: true},
+        {title: "temp", href: "/temp", isShow: true},
+    ];
+</script>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
---
+++ src/routes/blog/routes.js
@@ -0,0 +1,7 @@
+export const routes = [
+    {
+        id: 1,
+        title: "Мой первый блог",
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
+    },
+];
